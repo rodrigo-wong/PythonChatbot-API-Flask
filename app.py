84106b0688f37f07b28271113248803b7bd513f5
rodrigo-wong <wongmacr@gmail.com>
2023-12-27 10:31:41 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask, jsonify, request
+from chat import get_response
+from flask_cors import CORS
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+CORS(app, origins=os.environ.get('CORS_ORIGIN'))
+
+@app.route("/")
+def index_get():
+    return "hello"
+
+@app.post("/predict")
+def predict():
+    text = request.get_json().get("message")
+    response = get_response(text)
+    message = {"answer": response}
+    return jsonify(message)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ chat.py
@@ -0,0 +1,60 @@
+import os
+import random
+import json
+import torch
+from model import NeuralNet
+from nltk_utils import bag_of_words, tokenize
+from dotenv import load_dotenv
+load_dotenv()
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open('intents.json', 'r') as f:
+    intents = json.load(f)
+
+FILE = 'data.pth'
+data = torch.load(FILE)
+
+input_size = data['input_size']
+hidden_size = data['hidden_size']
+output_size = data['output_size']
+all_words = data['all_words']
+tags = data['tags']
+model_state = data['model_state']
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+bot_name = "Rodrigo"
+
+def get_response(message):
+    message = tokenize(message)
+    x = bag_of_words(message, all_words)
+    x = x.reshape(1, x.shape[0])
+    x = torch.from_numpy(x).to(device)
+
+    output = model(x)
+    _, predicted = torch.max(output, dim=1)
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent['tag']:
+                return random.choice(intent['responses'])
+    else:
+        return f"Could you please rephrase your question?"
+
+if __name__ == "__main__":
+    print("Let's chat! (type 'quit' to exit)")
+    while True:
+        # sentence = "do you use credit cards?"
+        sentence = input("You: ")
+        if sentence == "quit":
+            break
+
+        resp = get_response(sentence)
+        print(resp)
+        print(os.environ.get('CORS_ORIGIN'))
